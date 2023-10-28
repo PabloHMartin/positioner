ac0fcef1f3313ae45253fbb72b47d11ca504497b
Pablo Hernandez <pabloh@Pablos-MacBook-Pro.local>
2023-10-28 20:55:44 +0200

    Initial commit

---
+++ apps/positioner/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'positioner-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'positioner';
+}
